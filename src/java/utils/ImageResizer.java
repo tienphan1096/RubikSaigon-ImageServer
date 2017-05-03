dbd505bf53932647a13f01f09daebd8d26d88452
Tien <tienphan1096@gmail.com>
2017-05-03 00:15:30 -0600

    ImageResizer.createThumbnail() needs folderPath & fileName

--- src/java/utils/ImageResizer.java
+++ src/java/utils/ImageResizer.java
@@ -27,8 +27,28 @@ import org.imgscalr.Scalr.Method;
  */
 public class ImageResizer {
     
-    public static void resizeAndSave(String sourcePath, String destinationPath, int newWidth, int newHeight){
-
+    /**
+     * Method to create a thumbnail of an image, save it under thumbnail_<image name> in the same folder.
+     * 
+     * @param folderPath folder of the original image, thumbnail will be saved in this as well
+     * @param fileName file name of the image to be processed, including extension.
+     */
+    public static void processThumbnail(String folderPath, String fileName){
+        String sourcePath=folderPath+"\\"+fileName;
+        String destinationPath=folderPath+"\\thumbnail_"+fileName;
+        int newWidth=600;
+        int newHeight=450;
+        resizeAndSave(sourcePath, destinationPath, newHeight, newHeight);
+    }
+    
+    /**
+     * 
+     * @param sourcePath path of original image file, including file name & extension
+     * @param destinationPath path to save thumbnail, including file name & extension
+     * @param newWidth
+     * @param newHeight 
+     */
+    private static void resizeAndSave(String sourcePath, String destinationPath, int newWidth, int newHeight){
         try {
             Thumbnails.of(sourcePath)
                     .size(newWidth, newHeight)
